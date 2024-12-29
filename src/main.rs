b9094462693b640dfe243eb660290301c2c7a944
Rishabh Das <rdas@tutanota.com>
2024-12-29 16:33:30 +0530

    set up axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+	tracing_subscriber::fmt::init();
+
+	let app = Router::new().route("/", get(root));
+
+	let listener = TcpListener::bind("0.0.0.0:3000").await?;
+	axum::serve(listener, app).await?;
+	Ok(())
+}
+
+async fn root() -> &'static str {
+	"Hello, World!"
 }
